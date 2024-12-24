16db95dee55a1ceea97ac62bc312f17f13519fe5
EndritGashi20 <endrit.gash@student.uni-pr.edu>
2024-12-24 14:43:34 +0100

    inputconfiguration

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import './NewPlace.css';
+import Input from '../../shared/components/FormElements/Input';
+import {VALIDATOR_REQUIRE} from '../../shared/utils/validators'
+
 const NewPlace = () => {
   return (
     <form className="place-form">
+      <Input element="input" 
+      type="text" label="Title" 
+      validators={[VALIDATOR_REQUIRE()]} 
+      errorText="Plase enter valid title"/>
 
     </form>
   );
